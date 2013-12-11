0c2a6830ebcdac66d0fc87f0b923f268ac9a3e4d
Miguel C. Vega <amcvega@gmail.com>
2013-12-11 10:49:03 +0800

    Added filterQuery and findRecord

--- src/Entity/Postgres.hs
+++ src/Entity/Postgres.hs
@@ -1,4 +1,6 @@
-module Entity.Postgres where
+module Entity.Postgres
+       (module X)
+       where
 
 import Control.Applicative
 
@@ -15,40 +17,5 @@ import Database.PostgreSQL.Simple.ToField
 import Database.PostgreSQL.Simple.ToRow
 import Database.PostgreSQL.Simple.TypeInfo.Static
 
-
-instance FromField StoreVal where
-    fromField = storeParser
-
-
-
-storeParser f mbs
-    | t `elem` map typoid [text, varchar] = StoreText <$> fromField f mbs
-    | t == typoid date = StoreDay <$> fromField f mbs
-    | t `elem` map typoid [int8, int4, int2] =
-        StoreInt <$> fromField f mbs
-    | t `elem` map typoid [timestamptz] =
-        StoreUTCTime <$> fromField f mbs
-    | t `elem` map typoid [float8, float4] =
-        StoreDouble <$> fromField f mbs
-    | t == typoid numeric = StoreDouble <$> fromRational <$> fromField f mbs
-    | otherwise =  returnError ConversionFailed f $  "invalid oid: " ++ (show t)
-    where
-        t = typeOid f
-
-
-
-instance ToField StoreVal where
-    toField x = case x of
-        StoreInt i -> toField i
-        StoreText t -> toField t
-        StoreDay d -> toField d
-        StoreDouble d -> toField d
-        StoreUTCTime t -> toField t
-
-
-
-instance (Storeable a) => ToRow (Entity a) where
-    toRow = map (\(_,v) -> toField v) . toList . eVal
-
-
--- tableName x = storeName x
+import Entity.Postgres.Interface as X
+import Entity.Postgres.Instances as X
